e2a073858566a7b382c0f6ecbbdf3b0016106748
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-05-23 15:13:20 +0200

    first commit

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+import Spinner from "./Spinner";
+import { useCities } from "../contexts/CitiesContext";
+
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((array, city) => {
+    if (!array.map((element) => element.country).includes(city.country))
+      return [...array, { country: city.country, emoji: city.emoji }];
+    else return array;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
